a40d391aafdb1dddae324b3ba98ec33a1d3b331a
August Lilleaas <august.lilleaas@me.com>
2010-01-08 10:13:14 +0100

    It makes sense to put before delete.

--- rest.rb
+++ rest.rb
@@ -13,9 +13,9 @@ request = Net::HTTP::Get.new("/users/1")
 response = http.request(request)
 # As with POST, the data is in response.body.
 
-request = Net::HTTP::Delete.new("/users/1")
-response = http.request(request)
-
 request = Net::HTTP::Put.new("/users/1")
 request.set_form_data({"users[login]" => "changed"})
+response = http.request(request)
+
+request = Net::HTTP::Delete.new("/users/1")
 response = http.request(request)
