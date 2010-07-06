be6408acd6328080b9be980d0ce056cb8b0d4177
perze <perze.ababa@gmail.com>
2010-07-06 20:55:08 +0800

    adding line to specify Content-Type

--- post_form.rb
+++ post_form.rb
@@ -11,5 +11,6 @@ http = Net::HTTP.new(uri.host, uri.port)
 
 request = Net::HTTP::Post.new(uri.request_uri)
 request.set_form_data({"q" => "My query", "per_page" => "50"})
+#request["Content-Type"] = "application/json"
 
 response = http.request(request)
