d6fdfccf94ae585bf55ac6e8019bf97c7692e987
Perze Ababa <perze.ababa@gmail.com>
2010-07-07 00:43:18 +0800

    uncommented change, added header update blurb

--- post_form.rb
+++ post_form.rb
@@ -11,6 +11,8 @@ http = Net::HTTP.new(uri.host, uri.port)
 
 request = Net::HTTP::Post.new(uri.request_uri)
 request.set_form_data({"q" => "My query", "per_page" => "50"})
-#request["Content-Type"] = "application/json"
+
+# Tweak headers, removing this will default to application/x-www-form-urlencoded 
+request["Content-Type"] = "application/json"
 
 response = http.request(request)
