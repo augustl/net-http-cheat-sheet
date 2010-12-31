d50c32e29914cfc76903a9e89012eff47f2129cc
August Lilleaas <august.lilleaas@gmail.com>
2010-12-31 13:08:00 +0100

    Adding examples of SSL verification.

--- ssl_and_https.rb
+++ ssl_and_https.rb
@@ -1,9 +1,31 @@
 require "net/https"
 require "uri"
 
-uri = URI.parse("https://secure.com/")
+uri = URI.parse("https://ssltest7.bbtest.net")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
+
+# You can use a certificate to verify the server you're connecting to is the
+# server you indented to connect to.
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+store = OpenSSL::X509::Store.new
+
+# Use all system root certificates. Most setups have root certs for verisign,
+# entrust, thawte, etc installed.
+store.set_default_paths
+
+# You can also manually provide certs to the store. Download a cert for
+# ssltest7 at https://www.thawte.com/roots and provide the full path to
+# that file here.
+store.add_file("/path/to/thawte_Primary_Root_CA.pem")
+
+# Or add a OpenSSL Ruby object.
+store.add_cert(OpenSSL::X509::Certificate.new(File.read("/path/to/thawte_Primary_Root_CA.pem")))
+
+http.cert_store = store
+
+# You can also skip verification. That may be a bad idea, though, read more here:
+# http://www.rubyinside.com/how-to-cure-nethttps-risky-default-https-behavior-4010.html
 http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
 request = Net::HTTP::Get.new(uri.request_uri)
