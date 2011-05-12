c714152d8d443198ab3d104c8fbada28c1ab228b
August Lilleaas <august@augustl.com>
2011-05-12 11:43:13 +0200

    Adding request.body= example.

--- methods_and_rest.rb
+++ methods_and_rest.rb
@@ -6,16 +6,19 @@ http = Net::HTTP.new("api.restsite.com")
 
 request = Net::HTTP::Post.new("/users")
 request.set_form_data({"users[login]" => "quentin"})
-response = http.request(request)
-# Use nokogiri, hpricot, etc to parse response.body.
+# or
+request.body = '{"username":"quentin"}'
+response = http.request(request) # Use nokogiri, hpricot, etc to parse response.body.
+
 
 request = Net::HTTP::Get.new("/users/1")
-response = http.request(request)
-# As with POST, the data is in response.body.
+response = http.request(request) # As with POST, the data is in response.body.
+
 
 request = Net::HTTP::Put.new("/users/1")
 request.set_form_data({"users[login]" => "changed"})
 response = http.request(request)
+
 
 request = Net::HTTP::Delete.new("/users/1")
 response = http.request(request)
